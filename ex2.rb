b6631e8b8a0491596a548b465102fe469ed8d575
Omair <omairraza9@gmail.com>
2017-10-31 13:16:11 -0400

    all code works with the exception of book

--- ex2.rb
+++ ex2.rb
@@ -17,19 +17,19 @@ drama_rating = gets.to_i
 puts "On a scale of 1 - 5, how much do you like comedies?"
 comedy_rating = gets.to_i
 
-#If they rate documentaries four or higher, recommend the documentary
+#If they rate documentaries four or higher, recommend the documentary                           #checked code- worked
 if doc_rating >= 4
   puts "I recomend you watch the documentary #{documentary}"
   #If they rate documentaries 3 or lower but both comedies and dramas 4 or higher,
-  # recommend the dramedy.
-elsif doc_rating <= 3 || comedy_rating >= 4 && drama_rating >= 4
+  # recommend the dramedy.                                                                                 #code works
+elsif doc_rating <= 3 && comedy_rating >= 4 && drama_rating >= 4
   puts "I recomend you watch the dramedy #{dramedy}"
-  #If they rate only dramas 4 or higher, recommend the drama.
-elsif drama_rating >= 4 && doc_rating <=4 && comedy_rating <=4
+  #If they rate only dramas 4 or higher, recommend the drama.                                                  #code works
+elsif  doc_rating <=3 && comedy_rating <=3 && drama_rating >= 4
   puts "I recomend you watch the dramedy #{drama}"
   # If they rate just comedies 4 or higher, recommend the comedy. Otherwise, recommend a good book.
-elsif comedy_rating >= 4 && drama_rating <= 4 && doc_rating <=4
-  puts "I recomend you watch the comedy #{comedy}"
-else comedy_rating <= 4 && drama_rating <= 4 && doc_rating <=4
+elsif  doc_rating <=3 && comedy_rating <=4 && drama_rating >= 3                                               #code works
+  puts "I recomend you watch the comedy #{comedy}"                                                  #code works
+else  doc_rating <=3 && comedy_rating <=3 && drama_rating <= 3
   puts "I recomend you read a good book."
 end
